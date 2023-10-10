f0b50933d42181444de7d05e6cc18afe63f3485d
Damian Myerscough <Damian.Myerscough@gmail.com>
2023-10-09 20:44:08 -0700

    Add unit tests for teams

---
+++ tests/incidents/test_teams.py
@@ -0,0 +1,32 @@
+
+from oncall.api.models import Teams
+from datetime import datetime
+
+
+def test_querying_teams(app, db):
+    """
+    Test querying teams endpoint
+    """
+    client = app.test_client()
+
+    db.session.add(
+        Teams(name='test-team', team_id='ABC123', summary='', last_checked=datetime.now())
+    )
+    db.session.commit()
+
+    resp = client.get('/api/v1/teams')
+
+    assert resp.status_code == 200
+    assert resp.json == {'teams': [{'alias': None, 'id': 1, 'name': 'test-team'}]}
+
+
+def test_querying_teams_none_exist(app, db):
+    """
+    Test querying teams endpoint with no teams
+    """
+    client = app.test_client()
+
+    resp = client.get('/api/v1/teams')
+
+    assert resp.status_code == 200
+    assert resp.json == {'teams': []}
