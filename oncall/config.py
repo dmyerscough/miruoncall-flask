0847655bd38ca2939bba4ae504da39f8e93a7b44
Damian Myerscough <Damian.Myerscough@gmail.com>
2023-10-17 17:50:25 -0700

    Fix cors

--- oncall/config.py
+++ oncall/config.py
@@ -60,10 +60,7 @@ class DevelopmentConfig(BaseConfig):
     """
     DEBUG = True
 
-    CORS_ORIGINS = [
-        'http://127.0.0.1:3000',
-        'http://127.0.0.1:5000',
-    ]
+    CORS_ORIGINS = ["*"]
 
 
 class ProductionConfig(BaseConfig):
